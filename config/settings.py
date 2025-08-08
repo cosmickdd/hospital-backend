ac619137f5b9a43c9b80295d947275528980d556
utkarsh tiwari <utkarsh2313003@akgec.ac.in>
2025-08-08 06:39:34 +0530

    backend

--- config/settings.py
+++ config/settings.py
@@ -32,21 +32,9 @@ if SENTRY_DSN:
 import dj_database_url
 
 # Database
-if os.environ.get('DATABASE_URL'):
-    DATABASES = {
-        'default': dj_database_url.config(conn_max_age=600)
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ.get('POSTGRES_DB', 'hospital_db'),
-            'USER': os.environ.get('POSTGRES_USER', 'postgres'),
-            'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
-            'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
-            'PORT': os.environ.get('POSTGRES_PORT', '5432'),
-        }
-    }
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 
 # Redis cache
 CACHES = {
