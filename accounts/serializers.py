be3cc88cb6e19069f01b9a8625767b737d363f09
utkarsh tiwari <utkarsh2313003@akgec.ac.in>
2025-08-23 02:25:56 +0530

    error-fixed

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -20,13 +20,10 @@ class PasswordChangeSerializer(serializers.Serializer):
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    captcha_token = serializers.CharField(write_only=True, required=True)
     class Meta:
         model = User
         fields = ('username', 'email', 'password', 'is_doctor')
     def create(self, validated_data):
-        # Remove captcha_token before creating user
-        validated_data.pop('captcha_token', None)
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
